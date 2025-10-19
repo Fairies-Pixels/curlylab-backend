b9b802a8af86435467ccacc0403c34664235e752
Viktoria Dombrovskaya <spamovik123@yandex.ru>
2025-10-19 14:10:39 +0300

    init project

---
+++ src/main/kotlin/com/curlylab/curlylabback/CurlylabbackApplication.kt
@@ -0,0 +1,11 @@
+package com.curlylab.curlylabback
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CurlylabbackApplication
+
+fun main(args: Array<String>) {
+	runApplication<CurlylabbackApplication>(*args)
+}
---
+++ src/test/kotlin/com/curlylab/curlylabback/CurlylabbackApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.curlylab.curlylabback
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CurlylabbackApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
